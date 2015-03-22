5042a9d32c0fb17ca1e3dbd478372c5b300436a3
Jake Smith <jakehockey10@gmail.com>
2015-03-22 11:30:10 -0600

    using our mongo server and added semicolons

--- mongo.js
+++ mongo.js
@@ -1,15 +1,15 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 // dburl to a mongodb server hosted in the cloud (i.e., mongolab)
-var dburl = 'mongodb://yelp:1234@ds039960.mongolab.com:39960/yelp'
-var survey = require('./data/survey.json')
+var dburl = 'mongodb://cyberbullying:1234@ds041140.mongolab.com:41140/cyberbullying';
+var survey = require('./data/survey.json');
 
 // get db
-var db = require('monk')(dburl)
+var db = require('monk')(dburl);
 
 // set the database
-app.db = db
+app.db = db;
 
 // use jade as the view engine
 app.set('view engine', 'jade');
@@ -21,12 +21,12 @@ app.get('/list/survey', function(req, res) {
     res.render('listSurvey.jade', {
         survey: survey
     })
-})
+});
 
 var server = app.listen(3000, function() {
 
-    var host = server.address().address
-    var port = server.address().port
+    var host = server.address().address;
+    var port = server.address().port;
 
     console.log('App listening at http://%s:%s', host, port)
-})
+});
