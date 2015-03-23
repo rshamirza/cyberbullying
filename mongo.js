8756ab5e068a4c6366bb1a4bfa6fa3d343f8882b
Rsha Mirza <rsmi9152@colorado.edu>
2015-03-22 21:52:30 -0600

    Demo

--- mongo.js
+++ mongo.js
@@ -63,10 +63,11 @@ app.post('/results', function (req, res) {
 // set where the static contents are (e.g., css, js)
 app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(3000, function() {
+app.set('port', (process.env.PORT || 3000))
 
-    var host = server.address().address;
-    var port = server.address().port;
+var server = app.listen(app.get('port'), function() {
 
+    var host = server.address().address
+    var port = server.address().port
     console.log('App listening at http://%s:%s', host, port)
-});
+})
